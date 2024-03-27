50ce0c411a879efe18d72c57e4433c1f8735fcc6
Gregor <gregor.presker@gmail.com>
2024-03-27 22:08:29 +0100

    commiting changes

---
+++ diplomska_idelek/src/main/java/org/example/diplomska_idelek/DiplomskaIdelekApplication.java
@@ -0,0 +1,13 @@
+package org.example.diplomska_idelek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomskaIdelekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomskaIdelekApplication.class, args);
+	}
+
+}
---
+++ diplomska_idelek/src/test/java/org/example/diplomska_idelek/DiplomskaIdelekApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.diplomska_idelek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomskaIdelekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
